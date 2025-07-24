eb1403524806064643157e789559ce98cbe1dbac
APPLE <apple@Ramesh-IMac.local>
2025-07-24 23:16:03 +0530

    Added all 13 words, images, audio and adjusted image sizes

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const words = [
-    // For now, let's start with just one word.
-    // You will add more words, images, and audio files here later.
-    { text: "Apple", image: "images/apple.png", audio: "audio/apple.mp3" }
+    { text: "a book", image: "images/a book.png", audio: "audio/a book.mp3" },
+    { text: "a pencil", image: "images/a pencil.png", audio: "audio/a pencil.mp3" },
+    { text: "a pen", image: "images/a pen.png", audio: "audio/a pen.mp3" },
+    { text: "a crayon", image: "images/a crayon.png", audio: "audio/a crayon.mp3" },
+    { text: "a ruler", image: "images/a ruler.png", audio: "audio/a ruler.mp3" },
+    { text: "a bag", image: "images/a bag.png", audio: "audio/a bag.mp3" },
+    { text: "a table", image: "images/a table.png", audio: "audio/a table.mp3" },
+    { text: "a chair", image: "images/a chair.png", audio: "audio/a chair.mp3" },
+    { text: "an apple", image: "images/an apple.png", audio: "audio/an apple.mp3" },
+    { text: "an orange", image: "images/an orange.png", audio: "audio/an orange.mp3" },
+    { text: "an egg", image: "images/an egg.png", audio: "audio/an egg.mp3" },
+    { text: "an eraser", image: "images/an eraser.png", audio: "audio/an eraser.mp3" },
+    { text: "an elephant", image: "images/an elephant.png", audio: "audio/an elephant.mp3" }
+
 ];
 
 let currentWordIndex = 0;
