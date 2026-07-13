e7a39eb0c0dccb8de6e394fb1f2087c54320f41c
Ramesh kumar <ramu@Rameshs-iMac.local>
2026-07-13 18:01:07 +0530

    Add device-block message UI (Step C) + cache bump v29

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,6 +1,6 @@
 // Bump CACHE_VERSION whenever you deploy lesson content changes
 // that should invalidate cached audio/images for all users.
-const CACHE_VERSION = 'v28';
+const CACHE_VERSION = 'v29';
 const CACHE_LESSONS = `speakup-lessons-${CACHE_VERSION}`;
 const CACHE_SHELL   = `speakup-shell-${CACHE_VERSION}`;
 const CACHE_CDN     = `speakup-cdn-${CACHE_VERSION}`;
